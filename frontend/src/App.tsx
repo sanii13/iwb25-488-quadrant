cc9d7d4d8c00cfcb1b7874b6e15d08865268146c
rumesh02 <rumeshth20@gmail.com>
2025-08-27 01:02:20 +0530

    feature: user profiles
    separate user profiles for doctor and patient. edit details & change password

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import PatientProfile from "./pages/PatientProfile";
+import DoctorProfile from "./pages/DoctorProfile";
 import "./App.css";
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/patientProfile" element={<PatientProfile />} />
+          <Route path="/doctorProfile" element={<DoctorProfile />} />
         </Routes>
       </div>
     </Router>
